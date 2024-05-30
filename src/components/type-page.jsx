d9be834095c33e43757aa9072a0651bdfd3a47bb
doctorthe113 <code.doc113@gmail.com>
2024-05-30 23:29:57 +0600

    added more sample books

--- src/components/type-page.jsx
+++ src/components/type-page.jsx
@@ -2,6 +2,34 @@ import { useEffect, useRef, useState } from 'react';
 import Epub from 'epubjs';
 import './type-page.css';
 
+const bookURLMap = {
+  1: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/alices-adventures-in-wonderland-by-lewis-carroll.epub',
+  2: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/don-quijote-de-la-mancha-by-miguel-de-cervantes-saavedra.epub',
+  3: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/dracula-by-bram-stoker.epub',
+  4: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/frankenstein-or-the-modern-prometheus-by-mary-wollstonecraft-shelley.epub',
+  5: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/gullivers-travels-into-several-remote-nations-of-the-world-by-jonathan-swift.epub',
+  6: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/hamlet-prince-of-denmark-by-william-shakespeare.epub',
+  7: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/incidents-in-the-life-of-a-slave-girl-written-by-herself-by-harriet-a-jacobs.epub',
+  8: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/metamorphosis-by-franz-kafka.epub',
+  9: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/moby-dick-or-the-whale-by-herman-melville.epub',
+  10: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/oliver-twist-by-charles-dickens.epub',
+  11: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/pride-and-prejudice-by-jane-austen.epub',
+  12: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/romeo-and-juliet-by-william-shakespeare.epub',
+  13: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/the-adventures-of-sherlock-holmes-by-arthur-conan-doyle.epub',
+  14: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/the-adventures-of-tom-sawyer-complete-by-mark-twain.epub',
+  15: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/the-art-of-war-by-active-6th-century-bsunzi.epub',
+  16: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/the-complete-works-of-william-shakespeare-by-william-shakespeare.epub',
+  17: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/the-count-of-monte-cristo-by-alexandre-dumas-and-auguste-maquet.epub',
+  18: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/the-great-gatsby-by-fscott-fitzgerald.epub',
+  19: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/the-jungle-book-by-rudyard-kipling.epub',
+  20: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/the-odyssey-by-homer.epub',
+  21: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/the-social-cancer-a-complete-english-version-of-noli-me-tangere-by-josé-rizal.epub',
+  22: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/the-strange-case-of-dr-jekyll-and-mr-hyde-by-robert-louis-stevenson.epub',
+  23: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/the-thousand-and-one-nights-vol-i-by-lane-lane-poole-poole-and-harvey.epub',
+  24: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/through-the-looking-glass-by-lewis-carroll.epub',
+  25: 'https://cdn.jsdelivr.net/gh/doctorthe113/inkflow/sample-books/winnie-the-pooh-by-a-a-milne.epub',
+};
+
 const neg_to_zero = (num) => {
   if (num < 0) {
     return 0;
@@ -73,6 +101,11 @@ export default function Type() {
 
   let filteredText = text.replace(/[“”]/g, '"');
   let textArr = filteredText.split('');
+
+  const load_library_book = (e) => {
+    let bookURL = bookURLMap[e.target.value];
+    book_update(bookURL);
+  };
 
   const book_update = (bookPath) => {
     // bookPath =
@@ -203,31 +236,57 @@ export default function Type() {
       <div className='type-bar'>
         {/* prettier-ignore */}
         <div className='book-select'>
-          <label htmlFor='book-upload'><span>{bookName}</span></label>
+          <label className='book-selecter' htmlFor='book-upload'><span>{bookName}</span></label>
           <input
             id='book-upload'
             type='file'
             accept='.epub, .opf'
             onChange={(e) => book_update(e.target.files[0])}
           />
-          <span>
+          <select className='book-selecter' onChange={(e) => load_library_book(e)}>
+            <option value='' disabled selected hidden>or, choose a book from the library</option>
+            <option value='1'>Alice's adventures in wonderland</option>
+            <option value='2'>Don quijote</option>
+            <option value='3'>Dracula</option>
+            <option value='4'>Frankenstein; or, the Modern Prometheus</option>
+            <option value='5'>Gulliver's travel into the several remote nations of the world</option>
+            <option value="6">Hamlet prince</option>
+            <option value="7">Incidents in the life of a slave girl</option>
+            <option value="8">Metamorphosis</option>
+            <option value="9">Moby Dick</option>
+            <option value="10">Oliver Twist</option>
+            <option value="11">Pride and prejudice</option>
+            <option value="12">Romeo and Juliet</option>
+            <option value="13">The adventures of Sherlock Holmes</option>
+            <option value="14">The adventures of Tom Sawyer</option>
+            <option value="15">The art of war</option>
+            <option value="16">The complete works of William Shakespeare</option>
+            <option value="17">The count of Monte Cristo</option>
+            <option value="18">The great Gatsby</option>
+            <option value="19">The jungle book</option>
+            <option value="20">The odyssey</option>
+            <option value="21">The social cancer \(English version\)</option>
+            <option value="22">The strange case of Dr, Jekyll and Mr, Hyde</option>
+            <option value="23">The thousand and one nights</option>
+            <option value="24">Through the looking glass</option>
+            <option value="25">Winnie the pooh</option>
+          </select>
+          <div className='chapter-page-selecter'>
             Chapter:
-            <select name='chapter' onChange={(e) => chapter_update(e)}>
+            <select className='chapter-page-select' onChange={(e) => chapter_update(e)}>
               <option value='' disabled selected hidden>0</option>
               {book.map((chapter, index) => {
                 return <option value={index}>{index + 1}</option>;
               })}
             </select>
-          </span>
-          <span>
             Page:
-            <select name='page' onChange={(e) => page_update(e)}>
+            <select className='chapter-page-select' onChange={(e) => page_update(e)}>
               <option value='' disabled selected hidden>0</option>
               {pages.map((page, index) => {
                 return <option value={index}>{index + 1}</option>;
               })}
             </select>
-          </span>
+          </div>
         </div>
         <hr />
         <div className='stats'>
